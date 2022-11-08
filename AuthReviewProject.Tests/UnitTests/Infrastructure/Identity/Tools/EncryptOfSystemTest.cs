b96db50fefda9788bc792b0b5c422b477a6bee45
Martin Betancur <martinbetancur27@gmail.com>
2022-11-08 11:43:10 -0500

    Create Unit Test AdminController and EncriptOfSystem Tool

---
+++ AuthReviewProject.Tests/UnitTests/Infrastructure/Identity/Tools/EncryptOfSystemTest.cs
@@ -0,0 +1,26 @@
+﻿using Infrastructure.Identity.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AuthReviewProject.Tests.UnitTests.Infrastructure.Identity.Tools
+{
+    public class EncryptOfSystemTest
+    {
+        [Theory]
+        [InlineData("123", "a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3")]
+        [InlineData("abc", "ba7816bf8f01cfea414140de5dae2223b00361a396177a9cb410ff61f20015ad")]
+        [InlineData("ABC123", "e0bebd22819993425814866b62701e2919ea26f1370499c1037b53b9d49c2c8a")]
+        public void GetSHA256OfString_InsertString_And_ReturnSHA256(string password, string expected)
+        {
+            var encrypt = new EncryptOfSystem();
+
+            var result = encrypt.GetSHA256OfString(password);
+
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ AuthReviewProject.Tests/UnitTests/WebApi/Controllers/AdminControllerTest.cs
@@ -0,0 +1,249 @@
+﻿using AuthJWTWebAPI.Controllers;
+using Core.DTO.User;
+using Core.DTO.UserDTO;
+using Core.Entities.Auth;
+using Core.Interfaces.Services;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AuthReviewProject.Tests.UnitTests.WebApi.Controllers
+{
+    
+
+    public class AdminControllerTest
+    {
+        Mock<IUserService> _mockUserService;
+        Mock<IRoleService> _mockRoleService;
+        Mock<IRolesInUserService> _mockUserRoleService;
+        public AdminControllerTest()
+        {
+            _mockUserService = new Mock<IUserService>();
+            _mockRoleService = new Mock<IRoleService>();
+            _mockUserRoleService = new Mock<IRolesInUserService>();
+        }
+
+        private CreateUserWithRoleDTO CreateDefaultUserWithRoleDTO()
+        {
+            return new CreateUserWithRoleDTO
+            {
+                Name = "TestName",
+                UserName = "UsernameTest",
+                Password = "123",
+                idRole = 2
+            };
+        }
+
+        private CreateUserDTO CreateDefaultUser()
+        {
+            return new CreateUserDTO
+            {
+                Name = "TestName",
+                UserName = "UsernameTest",
+                Password = "123"
+            };
+        }
+
+        private UserRole CreateDefaultUserRole()
+        {
+            return new UserRole
+            {
+                Id = 1,
+                IdRole = 1,
+                IdUser = 1
+            };
+        }
+
+        private UserRoleDTO CreateDefaultUserRoleDTO()
+        {
+            return new UserRoleDTO
+            {
+                IdRole = 1,
+                IdUser = 1
+            };
+        }
+
+        [Fact]
+        public async Task AddUserWithRole_RoleNotExist_And_ReturnNotFound()
+        {
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(false));
+            _mockUserService.Setup(p => p.IsUsernameRegisteredAsync(It.IsAny<string>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddUserWithRole(CreateDefaultUserWithRoleDTO());
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task AddUserWithRole_UsernameExist_And_ReturnForbidCode()
+        {
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockUserService.Setup(p => p.IsUsernameRegisteredAsync(It.IsAny<string>())).Returns(Task.FromResult(true));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddUserWithRole(CreateDefaultUserWithRoleDTO()) as ObjectResult;
+
+            Assert.Equal(403, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddUserWithRole_UsernameExist_And_ReturnCreatedCode()
+        {
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockUserService.Setup(p => p.IsUsernameRegisteredAsync(It.IsAny<string>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+            var result = await _adminController.AddUserWithRole(CreateDefaultUserWithRoleDTO()) as ObjectResult;
+
+            Assert.Equal(201, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddUser_UsernameExist_And_ReturnForbidCode()
+        {
+            _mockUserService.Setup(p => p.IsUsernameRegisteredAsync(It.IsAny<string>())).Returns(Task.FromResult(true));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddUser(CreateDefaultUser()) as ObjectResult;
+
+            Assert.Equal(403, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddUser_And_ReturnCreatedCode()
+        {
+            _mockUserService.Setup(p => p.IsUsernameRegisteredAsync(It.IsAny<string>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddUser(CreateDefaultUser()) as ObjectResult;
+
+            Assert.Equal(201, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteUser_UserIdNotExist_And_ReturnNotFound()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.DeleteUser(It.IsAny<int>());
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteUser_And_ReturnOK()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.DeleteUser(It.IsAny<int>());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteRoleInUser_UserIdNotExist_And_ReturnNotFound()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.DeleteRoleInUser(CreateDefaultUserRoleDTO());
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteRoleInUser_RoleIdNotExist_And_ReturnNotFound()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.DeleteRoleInUser(CreateDefaultUserRoleDTO());
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteRoleInUser_RoleNotExistInUser_And_ReturnNotFound()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockUserRoleService.Setup(p => p.GetByUserAndRoleAsync(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.FromResult((UserRole)null));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.DeleteRoleInUser(CreateDefaultUserRoleDTO());
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task DeleteRoleInUser_And_ReturnOK()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockUserRoleService.Setup(p => p.GetByUserAndRoleAsync(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.FromResult(CreateDefaultUserRole()));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.DeleteRoleInUser(CreateDefaultUserRoleDTO());
+
+            Assert.IsType<OkObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task AddRoleInUser_UserIdNotExist_And_ReturnNotFound()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddRoleInUser(CreateDefaultUserRoleDTO());
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task AddRoleInUser_RoleIdNotExist_And_ReturnNotFound()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddRoleInUser(CreateDefaultUserRoleDTO());
+
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
+
+        [Fact]
+        public async Task AddRoleInUser_RoleExistInUser_And_ReturnForbidCode()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockUserRoleService.Setup(p => p.IsRoleAndUserAsync(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.FromResult((true)));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddRoleInUser(CreateDefaultUserRoleDTO()) as ObjectResult;
+
+            Assert.Equal(403, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddRoleInUser_And_ReturnCreatedCode()
+        {
+            _mockUserService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockRoleService.Setup(p => p.IsIdRegisteredAsync(It.IsAny<int>())).Returns(Task.FromResult(true));
+            _mockUserRoleService.Setup(p => p.IsRoleAndUserAsync(It.IsAny<int>(), It.IsAny<int>())).Returns(Task.FromResult(false));
+            var _adminController = new AdminController(_mockUserService.Object, _mockRoleService.Object, _mockUserRoleService.Object);
+
+            var result = await _adminController.AddRoleInUser(CreateDefaultUserRoleDTO()) as ObjectResult;
+
+            Assert.Equal(201, result.StatusCode);
+        }
+    }
+}
